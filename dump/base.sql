20adc798e416f1771c85c871c3b2b0330ed27461
Marius Cucuruz <marius.cucuruz@gmail.com>
2021-12-10 11:23:54 +0000

    fixes API composer installation

--- dump/base.sql
+++ dump/base.sql
@@ -15,10 +15,10 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-
--- create the databases
+--
+-- Create databases `invoice_headers` as hinted above.
+--
 CREATE DATABASE IF NOT EXISTS `interview_tests`;
-
 
 --
 -- Table structure for table `invoice_headers`
